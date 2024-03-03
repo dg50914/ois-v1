3a394d76609f7fe2487c9e4de95c382146353fa0
dg50914 <dg50914@student.uni-lj.si>
2024-03-03 16:04:28 +0100

    Dodal izracun pospeska

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 public class Gravitacija {
     public static void main(String[] args) {
-        System.out.println("OIS je zakon!");
+        Scanner sc = new Scanner(System.in);
+	double distance = sc.nextDouble();
+	double CM = 3.9857128 * (100000000000000L);
+	double r = 6.371 * 1000000;
+
+	double a = CM / Math.pow((r + distance), 2);
+	System.out.println(a);
     }
 }
